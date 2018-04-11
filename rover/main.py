f8fbf511d0b78e71fd983062adb63d6722e816c6
ericjunkins <eric.junkins@colorado.edu>
2018-04-11 11:58:01 -0700

    Added comments on main.py describing how to use the main script

--- rover/main.py
+++ rover/main.py
@@ -7,6 +7,23 @@ args = Arguments()
 conn = Connections()
 rover = Rover()
 
+'''
+
+This code runs the JPL Open Source Rover. It accepts a few command line arguments for different functionality
+   -t : Testing mode - allows you to test commands to send to the rover emulating a signals from a controller
+   -s : Attempts to connect to a Unix socket for controlling the LED screen. The screen.py script must be running
+   			previous to this in order to work. It lives at ../led/screen.py
+   -c : Controller flag, letting this program know what type of controller you wish to run with
+   		b : Bluetooth app (default)
+   		x : XBox controller (requires USB reciever)
+
+An example line running this script to run the LED screen and with an Xbox controller
+	sudo python main.py -s -c x
+
+'''
+
+
+
 def listener():
 	'''
 	Based on command line args decides which controller and sockets to open
